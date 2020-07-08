885afacd578c6d258ca08bf2f821ac7e8d29e814
Natalija Markvarte <natalija.markvarte@bbit.tech>
2020-07-08 19:12:35 +0300

    nAVIGATION FOR fLATS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HousesComponent } from './houses/houses.component';
 import { AddUpdateHousesComponent } from './add-update-houses/add-update-houses.component';
+import { FlatsComponent } from './flats/flats.component';
+import { AddUpdateFlatsComponent } from './add-update-flats/add-update-flats.component';
 
 
 const appRoutes: Routes = [
   {path: 'list', component: HousesComponent},
   {path: 'create', component: AddUpdateHousesComponent},
   {path: 'edit/:id', component: AddUpdateHousesComponent},
-  {path: '', redirectTo: '/list', pathMatch: 'full'}
+  {path: '', redirectTo: '/list', pathMatch: 'full'},
+  {path: 'flats/:houseId', component: FlatsComponent},
+  {path: 'createFlats/:houseId', component: AddUpdateFlatsComponent}
 ];
 
 @NgModule({
--- src/app/houses/houses.component.ts
+++ src/app/houses/houses.component.ts
@@ -4,6 +4,7 @@ import { House, DefaultHouse } from './house';
 import { HousesService } from '../houses.service';
 import * as _ from 'lodash';
 import { AddUpdateHousesComponent } from '../add-update-houses/add-update-houses.component';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -14,37 +15,40 @@ import { AddUpdateHousesComponent } from '../add-update-houses/add-update-houses
 export class HousesComponent implements OnInit {
   @Input() houses: Array<House>;
   public currentHouse: House;
-   constructor(private houseService: HousesService) { // don't sure it is necessary
-     houseService.get("Houses").subscribe((data: Array<House>) => this.houses = data); // take array from server
-     this.currentHouse = this.getDefaultHouse();
+  constructor(
+    private houseService: HousesService,
+    private router: Router
+    ) { // don't sure it is necessary
+    houseService.get("Houses").subscribe((data: Array<House>) => this.houses = data); // take array from server
+    this.currentHouse = this.getDefaultHouse();
     }
 
- // @Output() recordDeleted = new EventEmitter<House>(); // sent when click on delete ?
+  // @Output() recordDeleted = new EventEmitter<House>(); // sent when click on delete ?
   @Output() newClicked = new EventEmitter<House>(); // sent when click on new ?
   @Output() editClicked = new EventEmitter<House>(); // sent when click on edit ?
   showFlats: number = null; // initial value
- 
+
   private getDefaultHouse() {
     return new DefaultHouse();
   }
- //  public createUpdateHouse(house: House) {
- /*    let houseWithId = _.find(this.houses, (el => el.id === house.id));
-    if (houseWithId) {
-      const updateIndex = _.findIndex(this.houses, { id: houseWithId.id });
-      this.houseService.update(house, "Houses").subscribe(() => {
-        this.houses.splice(updateIndex, 1, house);
-      }
-      );
-    } else {
-      house.id = this.houses[this.houses.length - 1].id + 1; // solution for Internal server error
-      this.houseService.add(house, "Houses").subscribe(
-        () => {
-          //house.id = record.id;
-          this.houses.push(house)
-        }
-      );
-    }
-    this.currentHouse = this.getDefaultHouse(); */
+  //  public createUpdateHouse(house: House) {
+  /*    let houseWithId = _.find(this.houses, (el => el.id === house.id));
+     if (houseWithId) {
+       const updateIndex = _.findIndex(this.houses, { id: houseWithId.id });
+       this.houseService.update(house, "Houses").subscribe(() => {
+         this.houses.splice(updateIndex, 1, house);
+       }
+       );
+     } else {
+       house.id = this.houses[this.houses.length - 1].id + 1; // solution for Internal server error
+       this.houseService.add(house, "Houses").subscribe(
+         () => {
+           //house.id = record.id;
+           this.houses.push(house)
+         }
+       );
+     }
+     this.currentHouse = this.getDefaultHouse(); */
   // }
 
   public deleteH(data: House) {
@@ -53,35 +57,35 @@ export class HousesComponent implements OnInit {
       () => this.houses.splice(deleteIndex, 1)
     );
   }
-/*   public editH(data: House) {
-
-   console.log("Something happend ? ");
-   console.log("Data empty ? " + data.country); // no data not empty and function works
-   //this.editClicked.emit(data); // was Object.assign({}, data)
-
-  } */
- /*  public newH(data: House) { // new is blank for now
-    this.newClicked.emit(data);
-  } */
+  /*   public editH(data: House) {
+  
+     console.log("Something happend ? ");
+     console.log("Data empty ? " + data.country); // no data not empty and function works
+     //this.editClicked.emit(data); // was Object.assign({}, data)
+  
+    } */
+  /*  public newH(data: House) { // new is blank for now
+     this.newClicked.emit(data);
+   } */
 
   public getConnectedFlats(data: number) {
     this.showFlats = data;
-
+    this.router.navigate(['flats']);
   }
 
- // getHouses(): void {
+  // getHouses(): void {
   //  this.houseService.get().subscribe((houses : House) => this.housesValues = houses);
   // }
   // addhouse(name: string): void {
- //   name = name.trim();
- // if (!name) { return; }
- // this.houseService.add({name} ).subscribe(h => {
- //   this.housesValues = h;
- // })
- // }
+  //   name = name.trim();
+  // if (!name) { return; }
+  // this.houseService.add({name} ).subscribe(h => {
+  //   this.housesValues = h;
+  // })
+  // }
 
   ngOnInit() {
-   // this.getHouses();
+    // this.getHouses();
     //  this._httpService.get(this.accessPointUrl).subscribe(val => { // json returns from server
     //    this.housesValues = val.json();
     //  });
@@ -89,5 +93,5 @@ export class HousesComponent implements OnInit {
 
     this.houseService.get("Houses").subscribe((data: Array<House>) => this.houses = data); // take array from server
     this.currentHouse = this.getDefaultHouse();
-    }
+  }
 }
