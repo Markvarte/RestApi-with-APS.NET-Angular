bdb5740e66dfa6f42b9225fe826cc4a62521ca67
Natalija Markvarte <natalija.markvarte@bbit.tech>
2020-07-13 12:51:41 +0300

    rename field, now - currentHouseId

--- src/app/flats/flats.component.ts
+++ src/app/flats/flats.component.ts
@@ -22,7 +22,7 @@ export class FlatsComponent implements OnInit {
   flatIdForT: number = null; // for connected tenants
 
   @Input() flatsValues: Array<Flat>;
-  show: number; // for flat list / contains house id ? - yes
+  currentHouseId: number; // for flat list / contains house id ? - yes
   public currentFlat: Flat;
   public getConnectedTenants(data: number) {
     this.flatIdForT = data;
@@ -33,43 +33,28 @@ export class FlatsComponent implements OnInit {
     return new DefaultFlat();
   }
 
-  /*  public createUpdateFlat(flat: Flat) {
-     flat.houseId = +this.show; // inicialize in new object house id 
-     let flatWithId = _.find(this.flatsValues, (el => el.id === flat.id));
-     if (flatWithId) {
-       const updateIndex = _.findIndex(this.flatsValues, { id: flatWithId.id });
-       this.flatService.update(flat).subscribe(() => {
-         this.flatsValues.splice(updateIndex, 1, flat);
-       }
-       );
-     } else {
-       flat.id = this.flatsValues[this.flatsValues.length - 1].id + 1; // solution for Internal server error
-       this.flatService.add(flat).subscribe(
-         () => {
-           this.flatsValues.push(flat)
-         }
-       );
-     }
-     this.currentFlat = this.getDefaultFlat();
-   } */
-
-  public deleteF(data: Flat) {
+ /*  public deleteF(data: Flat) {
     const deleteIndex = _.findIndex(this.flatsValues, { id: data.id });
     this.flatService.remove(data).subscribe(
       () => {
         this.flatsValues.splice(deleteIndex, 1);
-        location.reload(); // but "would completely destroy all stored states, including WebSocket connections." >>
+        this.ngOnInit();
+        this.router.navigateByUrl('/createFlats' + this.currentHouseId, { skipLocationChange: true }).then(() => { // seems not working
+          this.router.navigate(['flats', this.currentHouseId]);
+          // source -> https://stackoverflow.com/questions/47813927/how-to-refresh-a-component-in-angular
+      });
+        location.reload();  // but "would completely destroy all stored states, including WebSocket connections." >>
       // source -> https://github.com/vuejs/vue-router/issues/311
       // + there still is a problem, when delete is pressed more that 1 time
       }
     );
   }
-
+ */
   ngOnInit() {
     this.route.params.subscribe(param => {
-      this.show = +param.houseId;
+      this.currentHouseId = +param.houseId;
     });
-    this.flatService.getByHouseId(this.show).subscribe(
+    this.flatService.getByHouseId(this.currentHouseId).subscribe(
       (data: Array<Flat>) => {
         this.flatsValues = data;
         console.log(data);
