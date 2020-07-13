b009376c9fe61374af824fc77e8b29e8fbf53546
Natalija Markvarte <natalija.markvarte@bbit.tech>
2020-07-13 18:17:22 +0300

    First routing for tenants

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AddUpdateHousesComponent } from './add-update-houses/add-update-houses.
 import { FlatsComponent } from './flats/flats.component';
 import { AddUpdateFlatsComponent } from './add-update-flats/add-update-flats.component';
 import { DeleteFlatsComponent } from './delete-flats/delete-flats.component';
+import { TenantsComponent } from './tenants/tenants.component';
 
 
 const appRoutes: Routes = [
@@ -12,14 +13,18 @@ const appRoutes: Routes = [
   {path: 'create', component: AddUpdateHousesComponent},
   {path: 'edit/:id', component: AddUpdateHousesComponent},
   {path: '', redirectTo: '/list', pathMatch: 'full'},
-  {path: 'flats/:houseId', component: FlatsComponent},
+  {path: 'flats/:houseId', component: FlatsComponent, children: [
+    {path: 'ten', component: TenantsComponent}
+  ]},
+ // {path: 'tenants', component: TenantsComponent},
   {path: 'createFlats/:houseId', component: AddUpdateFlatsComponent},
   {path: 'editFlats/:id/:houseId', component: AddUpdateFlatsComponent},
   {path: 'deleteFlats/:id/:houseId', component: DeleteFlatsComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes),
+           RouterModule.forChild(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
